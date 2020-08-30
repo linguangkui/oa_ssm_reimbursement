a3cbd5590ea5ab2839bd9e291c11d77f6c82668a
linguangkui <linguangkui@163.com>
2020-08-30 19:53:11 +0800

    处理中文乱码

--- output/oa_ssm_reimbursement/src/com/web/oa/controller/WorkFlowController.java
+++ output/oa_ssm_reimbursement/src/com/web/oa/controller/WorkFlowController.java
@@ -151,6 +151,7 @@ public class WorkFlowController {
 	@RequestMapping("/delDeployment")
 	public void delDeployment(String deploymentId,HttpServletResponse response) throws Exception{
 		PrintWriter out = response.getWriter();
+		response.setContentType("text/html;charset=UTF-8");
 		//使用部署对象ID，删除流程部署对象
 		workFlowService.deleteProcessByDeploymentId(deploymentId);
 		out.write("<script type=\"text/javascript\">");
